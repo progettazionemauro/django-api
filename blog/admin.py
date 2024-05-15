09be3afb86a06c80d95d8eae97c17c515d5c2abc
progettazionemauro <progettazione@studiogb.org>
2024-05-15 16:58:34 +0200

    Added blog directory

---
+++ blog/admin.py
@@ -0,0 +1,26 @@
+
+from django.contrib import admin
+from .models import CustomFeature
+import os
+import subprocess
+
+@admin.register(CustomFeature)
+class CustomFeatureAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+    def run_script(self, request, queryset):
+        try:
+            # This line retrieves the directory path of the current Python script (admin.py in this case) using __file__, 
+            # which is a special attribute in Python that represents the current file path.
+            current_dir = os.path.dirname(__file__) 
+            script_path = os.path.abspath(os.path.join(current_dir, 'add_page.sh'))
+            print("Absolute path to script:", script_path)  # Print out the absolute path
+            subprocess.run([script_path], check=True)
+            self.message_user(request, "Script executed successfully")
+        except Exception as e:
+            self.message_user(request, f"Script execution failed: {e}", level='ERROR')
+
+
+    run_script.short_description = "Run add_page.sh"
+
+    actions = [run_script]
