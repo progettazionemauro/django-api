88b0f7cd88be5b50befc3ebb7422b0aae9576291
progettazionemauro <progettazione@studiogb.org>
2024-05-15 16:31:10 +0200

    Updated project files (excluding cheatsheet.md)

--- django_api_for_wagtail/models.py
+++ django_api_for_wagtail/models.py
@@ -7,9 +7,3 @@ class Nation(models.Model):
     def __str__(self):
         return self.name
 
-class CustomFeature(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    description = models.TextField()
-
-    def __str__(self):
-        return self.name
